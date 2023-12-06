c4cc642cd2aa41360bca89797b27f4bb4ef40d77
snirEvgi <email>
2023-12-06 18:25:37 +0200

    testing the geolocation

--- weather-app/src/app/pages/HomePage/homepageAPI.ts
+++ weather-app/src/app/pages/HomePage/homepageAPI.ts
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-const locationSearch = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=ICfOrVGI3ofdnGODMlLrRMwyPbISOCdO&q=Los%20Angeles"
-const apiKey3 = "XqAwKjl5vHX6rEFkdbfLq7zj9yHz7o4R"//done
+
+export async function fetchTelAvivData() {
+  const apiKey3 = "XqAwKjl5vHX6rEFkdbfLq7zj9yHz7o4R"//done
 const apiKey = "ICfOrVGI3ofdnGODMlLrRMwyPbISOCdO"
 const apiKey4= "9qrvieMrQl23ieBw1AAmjW9vLTxeunvF"
 const apiKey2 = "AgSwRsJttx2l9xFP9UVZ1M9l3VSkfR5I"
-export async function fetchTelAvivData() {
     const basedLocationKeySearchFiveDayForecast = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/215854?apikey=${apiKey}`
     const basedLocationKeySearchCurrentForecast = `http://dataservice.accuweather.com/currentconditions/v1/215854?apikey=${apiKey}`
   try {
