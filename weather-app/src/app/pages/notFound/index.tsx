b98cf4e7db5a80461d6396facf8a475bf4ca4c4d
snirEvgi <email>
2023-12-06 16:00:40 +0200

    404 page tailwind css update

--- weather-app/src/app/pages/notFound/index.tsx
+++ weather-app/src/app/pages/notFound/index.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import './NotFound.css';
 import notFoundImage from './not-found-image.png';
 
 export default function NotFound() {
-  const [isSupportOn, setIsSupportOn] = useState<boolean>(false)
-useEffect(()=>{
-  document.title = `404 Not Found`
-},[])
+  const [isSupportOn, setIsSupportOn] = useState<boolean>(false);
+
+  useEffect(() => {
+    document.title = `404 Not Found`;
+  }, []);
+
   const handleSupportChat = async () => {
-    setIsSupportOn(!isSupportOn)
+    setIsSupportOn(!isSupportOn);
   };
+
   return (
-    <div className="notFoundContainer">
-      <div className="notFoundContent">
-        <h1 className="notFoundHeader">404</h1>
-        <h2 className="notFoundSubheader">Page Not Found</h2>
-        <p className="notFoundMessage">
+    <div className="flex justify-center font-bold items-center bg-slate-50 bg-opacity-25 min-h-screen ">
+      <div className="text-center">
+        <h1 className="text-6xl mb-3 text-red-600">404</h1>
+        <h2 className="text-2xl text-gray-700">Page Not Found</h2>
+        <p className="text-base text-black mt-1">
           Sorry, the page you are looking for might be in another universe.
         </p>
       </div>
